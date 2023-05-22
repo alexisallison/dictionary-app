1d84d2610635b925e72b6fd7e604a98423313d26
alexisallison <alexisallison1801@gmail.com>
2023-05-22 19:24:34 -0400

    Processed results from API by creating Results component and Meaning

---
+++ src/Meaning.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <p>
+        {" "}
+        {props.meaning.definition}
+        <br />
+        <br />
+        <em>{props.meaning.example}</em>
+      </p>
+    </div>
+  );
+}
