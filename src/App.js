31bb4eef7e3d1e059b8357091ce188a320432062
alexisallison <alexisallison1801@gmail.com>
2023-05-22 17:28:58 -0400

    added footer and basic search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <Dictionary />
+        <footer className="text-center">Coded by Alexis Allison</footer>
+      </div>
     </div>
   );
 }
