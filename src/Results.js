da44e83288522ce5fbbfb815275dcea8a5327c03
alexisallison <114777162+alexisallison@users.noreply.github.com>
2023-06-03 17:22:37 -0400

    added sections, font families, usestate loaded

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     console.log(props.results);
     return (
       <div className="Results">
+        <section>
         <h2>{props.results.word}</h2>
-        <h5>/{props.results.phonetic}/</h5>
+        <h5>/{props.results.phonetic}/</h5></section>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
-              <Meaning meaning={meaning} />
+              <section>
+              <Meaning meaning={meaning} /></section>
             </div>
           );
         })}
